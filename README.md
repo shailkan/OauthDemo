66faf5747842cade1d8ae67a5fa5707c8246ec64
Shaily Aggarwal <shaily93@gmail.com>
2021-01-31 00:02:36 +0530

    Add project files

--- README.md
+++ README.md
@@ -1,2 +1,5 @@
 # OauthDemo
 Demo Application for Github oauth
+
+<i>Reference:</i> 
+https://spring.io/guides/tutorials/spring-boot-oauth2/
---
+++ src/test/java/com/oathDemo/oathDemo/OathDemoApplicationTests.java
@@ -0,0 +1,13 @@
+package com.oathDemo.oathDemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OathDemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
